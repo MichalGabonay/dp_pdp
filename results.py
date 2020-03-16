3d3b9933066ba9194b844ce6177f8f884dc08b61
MichalGabonay <michal.gabonay@wereldo.com>
2020-03-16 22:43:04 +0100

    update result script

--- results.py
+++ results.py
@@ -1,11 +1,12 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import glob, os
 
+SUFFCIENT_FITNESS = 0.426 # cca 5% worse than Or-tools LC1_2_1
+
 def Average(lst):
     return sum(lst) / len(lst)
-
 
 results = {}
 for file in glob.glob('./*.[0-9]'):
@@ -16,7 +17,7 @@ for file in glob.glob('./*.[0-9]'):
         for row in csv_reader:
             if results.get(row[0]) == None:
                 results.update(
-                    {row[0]: {"bests": [], "averages": [], "generations": []}})
+                    {row[0]: {"bests": [], "averages": [], "generations": [], "best": 0}})
             population = []
             for i in range(1, len(row)):
                 population.append(float(row[i]))
@@ -29,6 +30,8 @@ bests = []
 boxplot_x = []
 boxplot_labels = []
 
+suffcient_solutions = 0
+
 gen_index = 0
 for key in results:
     generation = 1000
@@ -37,6 +40,8 @@ for key in results:
     for gen in results[key]["generations"]:
         results[key]["bests"].append(max(gen))
         results[key]["averages"].append(Average(gen))
+        if max(gen) > results[key]["best"]:
+            results[key]["best"] = max(gen)
         axis_x.append(generation)
         generation += 1000
         if gen_index == 0:
@@ -47,6 +52,8 @@ for key in results:
         # if generation%1000000 == 0:
         #     boxplot_x.append(gen)
         #     boxplot_labels.append(generation)
+    if results[key]["best"] >= SUFFCIENT_FITNESS:
+        suffcient_solutions += 1
     gen_index += 1
 
     # fig = plt.figure(1, figsize=(16, 12))
@@ -77,14 +84,14 @@ for gen_of_bests in bests:
             boxplot_labels.append(generation)
     generation += 1000
     
-# fig = plt.figure(1, figsize=(16, 12))
-# plt.plot(axis_x, best_of_bests)
-# plt.plot(axis_x, avrgs)
-# plt.plot(axis_x, worst_of_bests)
-# plt.ylabel('fitness')
-# plt.xlabel('generations')
-# plt.savefig('bests.png')
+fig = plt.figure(1, figsize=(16, 12))
+plt.plot(axis_x, best_of_bests)
+plt.plot(axis_x, avrgs)
+plt.plot(axis_x, worst_of_bests)
+plt.ylabel('fitness')
+plt.xlabel('generations')
+plt.savefig('bests.png')
 
-# plt.cla()
+plt.cla()
 
-print("BEST: ", max(best_of_bests), ", AVRG: ", max(avrgs), ", WORST: ", max(worst_of_bests))
+print("BEST: ", max(best_of_bests), ", AVRG: ", max(avrgs), ", WORST: ", max(worst_of_bests), ", SUFFCIENT: ", (suffcient_solutions/len(results))*100, "%")
